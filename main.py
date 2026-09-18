feaf9e458ba1b442313f319d4a7ebfc7eb3781cf
simonlopez25 <simonlopezsaldarroaga@gmail.com>
2026-09-18 12:13:12 +0200

    feat: add catalog viewing menu for full or individual item lookup

--- main.py
+++ main.py
@@ -3,21 +3,22 @@ print("¡Acabas de ingresar al mejor catálogo de piezas coleccionables!")
 
 parts_inventory = []
 
-for i in range(1,2):
+for i in range(1, 2):
 
     print(f"\nIngresado los datos de la pieza numero {i}")
     name = input("Ingrese su nombre: ")
     category = input("Ingrese la categoria de la pieza: ")
     price = float(input("Ingrese el precio de la pieza: "))
 
-    status = input("Selecciona el estado de la pieza: (Disponible,Reservada,Vendida) ")
-    while status not in ["Disponible", "Reservada", "Vendida"]:
-        print("Estatus invalido")
+    status = input("Selecciona el estado de la pieza: (disponible,reservada,vendida) ")
+    while status not in ["disponible", "reservada", "vendida"]:
+        print("estatus,invalido")
         status = input("Selecciona el estado de la pieza: ")
 
     description = input("Ingrese la descripcion de la pieza (Usada,Certificada)  ")
-    while description not in ["Usada", "Certificada"]:
-        status = input("Descripcion invalido: ")
+    while description not in ["usada", "certificada"]:
+        print("descripcion,invalido")
+        description = input("descripcion invalido digita la descripcion estipulada: ")
 
     parts = {
         "id": i,
@@ -27,10 +28,25 @@ for i in range(1,2):
         "status": status,
         "description": description
     }
-    "APPEND ME SIRVE PARA REGISTRAR CORRECTAMENTE LOS DATOS"
+
     parts_inventory.append(parts)
-
     print("Registro exitoso")
-    print(f"Se han registrado {len(parts_inventory)} piezas")
 
+selected_view = input("\n1. Ver todas las piezas / 2. Ver una pieza individual: ")
 
+if selected_view == "1":
+    print("Catalogo completo")
+    for item in parts_inventory:
+        print(item)
+
+elif selected_view == "2":
+    item_position = int(input(f"Ingrese la posicion del pieza (1 al {len(parts_inventory)}): "))
+
+    if 1 <= item_position <= len(parts_inventory):
+        index = item_position - 1
+        print("Detalles del pieza:")
+        print(parts_inventory[index])
+    else:
+        print("Posicion fuera del rango")
+else:
+    print("Opcion invalido")
